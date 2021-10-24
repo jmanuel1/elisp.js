267771798318d82f06607830fe5d7551f50cecdc
Jason Manuel <jama.indo@hotmail.com>
2021-10-24 01:21:58 -0700

    Add `&rest` argument support directly to elisp.js

--- elisp/elisp.js
+++ elisp/elisp.js
@@ -12,19 +12,38 @@ async function fcall(args, env) {
   // TODO: Combine argument logic with subr argument validation
   let expected_args_length = this.args.length;
   const optional_start_index = this.args.indexOf('&optional');
+  const has_rest = this.args.length >= 2 && this.args[this.args.length - 2] === '&rest';
   if (optional_start_index > -1) {
     expected_args_length = optional_start_index;
     if (args.length < expected_args_length) {
       throw new ty.LispError('Wrong number of arguments: ' + this.to_string() + ', ' + args.length);
     }
-    let optional_arguments = args.slice(optional_start_index);
+    let optional_argument_count;
+    if (has_rest) {
+      optional_argument_count = this.args.length - 2 - (optional_start_index + 1);
+    } else {
+      optional_argument_count = this.args.length - (optional_start_index + 1);
+    }
+    let optional_arguments = args.slice(optional_start_index, optional_argument_count + optional_start_index);
     const nil_arguments = new Array(this.args.length - 1 - optional_arguments.length);
     nil_arguments.fill(ty.nil);
-    optional_arguments = optional_arguments.concat(nil_arguments);
-    args = args.concat(optional_arguments);
+    args = args.concat(nil_arguments);
   }
-  if (args.length != this.args.length)
+  let new_args;
+  let previous_args_end_index = this.args.length - 2;
+  if (optional_start_index > -1) {
+    previous_args_end_index--;
+  }
+  if (has_rest) {
+    new_args = args.slice(0, previous_args_end_index);
+    new_args.push(ty.list(args.slice(previous_args_end_index)));
+  } else {
+    new_args = args;
+  }
+  if (optional_start_index < 0 && !has_rest && args.length != this.args.length)
     throw new ty.LispError('Wrong number of arguments: ' + this.to_string() + ', ' + args.length);
+
+  const new_argspec = this.args.filter(arg => !['&optional', '&rest'].includes(arg));
 
   let body = this.body;
   if (body.is_false) {
@@ -37,14 +56,14 @@ async function fcall(args, env) {
     body = ty.cons(ty.symbol('progn'), body);
   }
 
-  this.jscode = await translate.lambda(this.args, body, env);
+  this.jscode = await translate.lambda(new_argspec, body, env);
   this.func = eval(this.jscode);
 
   try {
-    env.push.call(env, this.args, args);
+    env.push.call(env, new_argspec, new_args);
     var result = await Promise.resolve(this.func());
   } finally {
-    env.pop.call(env, this.args);
+    env.pop.call(env, new_argspec);
   }
   return result;
 }
